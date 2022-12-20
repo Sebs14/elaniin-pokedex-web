810826f8e4a334777dec25de8690d21781e336e4
Sebastian <sebastian.flores@elaniin.com>
2022-12-19 22:20:01 -0600

    feat: :sparkles: Log In working properly on auth
    Added a context in order to be used on the auth for every user, changes on the navbar and UserNavbar, created a firebase config file and  completed funtionalities on some components

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,21 @@
 import "../styles/globals.css";
 import Head from "next/head";
-
+import { AuthContextProvider } from "./UserContext";
+import { useState, useMemo} from 'react';
 
 function MyApp({ Component, pageProps }) {
+
+  
   return (
-      <>
+    <>
+      <AuthContextProvider>
         <Head>
           <title>Pokedex</title>
         </Head>
-        
         <Component {...pageProps} />
-      </>
-    )
+      </AuthContextProvider>
+    </>
+  );
 }
 
 export default MyApp;
